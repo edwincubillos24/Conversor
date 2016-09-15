f0cab89eb486987741e4bc5c93edb8c5d5f06e91
Edwin <edwinacubillos@gmail.com>
2016-09-14 20:28:51 -0500

    Version 1.0 Conversor funcionando

--- app/src/main/java/com/edwinacubillos/conversor/MainActivity.java
+++ app/src/main/java/com/edwinacubillos/conversor/MainActivity.java
@@ -2,12 +2,32 @@ package com.edwinacubillos.conversor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText ePesos, eDolares;
+    Button bConvertir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ePesos = (EditText) findViewById(R.id.ePesos);
+        eDolares = (EditText) findViewById(R.id.eDolares);
+        bConvertir = (Button) findViewById(R.id.bConvertir);
+
+        bConvertir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Double pesos, dolares;
+                pesos = Double.parseDouble(ePesos.getText().toString());
+                dolares = pesos * 2900;
+                eDolares.setText(dolares.toString());
+            }
+        });
     }
 }
